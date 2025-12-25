a844bbc99ffff76fde06e80cec51f24c848e8184
Innovation <infoatanish@gmail.com>
2025-12-25 08:56:09 +0545

    add to head and tail in DLL

--- LinkedList_chap/doubly_linklist.java
+++ LinkedList_chap/doubly_linklist.java
@@ -1,46 +1,80 @@
-//creatiion of doubly linked list
+//Add to head and tail in doubly linked list
 
 import java.util.Scanner;
+
 public class doubly_linklist {
-    static class Node{
+
+    static class Node {
         int data;
         Node next;
-        Node prev; //pointer to previous node    
+        Node prev;
 
-    //constructor 
-    Node (int data){
-        this.data = data;
-        this.next = null;
-        this.prev = null;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
     }
-}   
-    Node head = null;
 
-    public void creation(){
-        int data,n;
-        Scanner sc = new Scanner (System.in);
-        do{
+    Node head = null;
+    Node tail = null;
+
+    // Insert at head
+    public void insertAtHead(int data) {
+        Node new_node = new Node(data);
+
+        if (head == null) {
+            head = new_node;
+            tail = new_node;
+        } else {
+            new_node.next = head;
+            head.prev = new_node;
+            head = new_node;
+        }
+    }
+
+    //  Insert at tail
+    public void insertAtTail(int data) {
+        Node new_node = new Node(data);
+
+        if (head == null) {
+            head = new_node;
+            tail = new_node;
+        } else {
+            tail.next = new_node;
+            new_node.prev = tail;
+            tail = new_node;
+        }
+    }
+
+    //  creation method using insertAtHead
+    public void creation() {
+        int data, n;
+        Scanner sc = new Scanner(System.in);
+
+        do {
             System.out.println("Enter data: ");
             data = sc.nextInt();
-            Node new_node = new Node(data);
 
-            if (head == null){
-                head = new_node;
-            }else{
-                new_node.next = head;
-                head.prev = new_node; //new 
-                head = new_node;
-            }
-            System.out.println("Do you want to continue, If yes press 1 : ");
+            insertAtHead(data);  
+
+            System.out.println("Do you want to continue? If yes press 1: ");
             n = sc.nextInt();
+
+        } while (n == 1);
+    }
+    //display method
+    public void display(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data + " ");
+            temp = temp.next;
         }
-        while(n==1);
-        sc.close();
     }
 
     public static void main(String[] args) {
         doubly_linklist list = new doubly_linklist();
         list.creation();
-        // list.display();
+        list.display();
     }
 }
